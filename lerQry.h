9673de8da51c43bdc036c9c7761b9945d2dc0a40
Eduarda <susseleduarda@gmail.com>
2026-07-08 00:26:20 -0300

    .h

---
+++ lerQry.h
@@ -0,0 +1,9 @@
+#ifndef LERQRY_H
+#define LERQRY_H
+
+#include "arvore.h"
+#include "lista.h"
+
+void lerArquivoQry(char *bed, char *nomeArq, char *pathSaida, char *nomeGeoOrig, Arvore bancoDeDados, Lista listaPoligonos);
+
+#endif
